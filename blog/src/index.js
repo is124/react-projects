d8588ee3b4bb8041d1605e4159b6ec82d72aa163
Tilak <1ms17s124@gmail.com>
2020-07-02 17:18:52 +0530

    For re-usability CommentDetail.js created

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 
 const App = () => {
     return (
             <div className = "ui container comments">
                 {/* Single Comment*/}
-                <div className="comment">
-
-                    <a href="/" className="avatar"><img alt="avatar" src={faker.image.avatar()}/></a>
-
-                    <div className='content'>
-                        <a href="/" className="author">Tilak</a>
-
-                        <div className='metadata'>
-                            <span className='date'>Time is 6:45 PM</span>
-                        </div>
-
-                        <div className='text'>Nice blog Post!!</div>
-
-                    </div>
-                </div>
+                < CommentDetail />
+                < CommentDetail />
+                < CommentDetail />
+                
             </div>
     );
 };
