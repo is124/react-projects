7bb5bd83a1d46c4ef85e1017aab36d2dec34e79f
Tilak <tilaks484@gmail.com>
2020-07-07 15:17:03 +0530

    Initial setup

---
+++ season-check/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>{
+    return <div>Working Pretty well !!</div>
+}
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
